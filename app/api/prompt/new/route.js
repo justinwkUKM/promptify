cf3d0f901601e8e0dfe96a31e3bfa9095b489c69
Waqas Obeidy <WObeidy@LT-Waqas-Obeidy.local>
2023-05-29 01:44:52 +0800

    langchain added-saving to db

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -30,7 +30,7 @@ export const POST = async (request) => {
     await connectToDB();
     const newPrompt = new Prompt({ creator: userId, prompt, optimized_prompt, tag });
 
-    // await newPrompt.save();
+    await newPrompt.save();
     return new Response(JSON.stringify(newPrompt), { status: 201 });
   } catch (error) {
     return new Response("Failed to create a new prompt", { status: 500 });
